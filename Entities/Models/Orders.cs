08d9925bc048fd75bba25eed2f0d0dea9390bc5c
Ritesh Parekh <ritesh_rhce@yahoo.co.in>
2021-12-29 18:27:23 +0530

    Added Entity Order

---
+++ Entities/Models/Orders.cs
@@ -0,0 +1,30 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Entities.Models
+{
+    public class Orders: BaseEntity
+    {
+
+        #region Constructors
+
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int OrderId { get; set; }
+
+        #endregion
+
+        #region Fields
+
+        [Display(Name = "Order Date")]
+        [Required(ErrorMessage = "Order Date Required")]
+        public DateTime OrderDate { get; set; }
+
+        [Display(Name = "TotalQuantity")]
+        [Required(ErrorMessage = "Total Quantity Required")]
+        public int TotalQuantity { get; set; } 
+
+        #endregion
+
+    }
+}
